e57dfd61e58a723627a937fe90ab1eb53e79c368
Insolor <Insolor@localhost>
2015-02-24 04:34:10 +0300

    edit_relocs.py: added warn about extra arguments for '-*' operation

--- edit_relocs.py
+++ edit_relocs.py
@@ -52,10 +52,11 @@ else:
                 relocs.discard(items)
             elif op == '-*':
                 if len(items) < 2:
-                    print('"-*" operation needs at least 2 arguments. Operation skipped.')
+                    print('"-*" operation needs 2 arguments. Operation skipped.')
                     continue
-                lower_bound = items[0]
-                upper_bound = items[1]
+                elif len(items) > 2:
+                    print('"-*" operation needs only 2 arguments. Using only two of them: 0x%x, 0x%x.' % tuple(items[:2]))
+                lower_bound, upper_bound = items[:2]
                 relocs = set(filter(lambda x: not (lower_bound <= x <= upper_bound), relocs))
             else:
                 print('Wrong operation: "%s". Skipped.' % cmd[2])
