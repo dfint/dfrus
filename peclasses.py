11657afd84f6a6277581c81802249466f8cae6c8
Mike Manturov <insolor@users.noreply.github.com>
2015-09-09 16:03:51 +0300

    Implement reading of the relocation table

--- peclasses.py
+++ peclasses.py
@@ -1,6 +1,7 @@
 #! python3
 import struct, bisect
 from collections import OrderedDict, namedtuple
+from array import array
 
 
 class ImageDosHeader:
@@ -163,7 +164,7 @@ class SectionTable(list):
         self._rvas = rvas
 
     @classmethod
-    def read(cls, offset, number):
+    def read(cls, file, offset, number):
         file.seek(offset)
         return cls([Section.read(file) for _ in range(number)])
 
@@ -184,22 +185,68 @@ class SectionTable(list):
             return None
 
 
+IMAGE_REL_BASED_ABSOLUTE = 0
+IMAGE_REL_BASED_HIGHLOW = 3
+
+
+class RelocationTable:
+    def __init__(self, raw_table):
+        self.raw = raw_table
+        self.plain = self._raw_to_plain(self.raw)
+
+    @staticmethod
+    def _raw_to_plain(raw_table):
+        for cur_page, records in raw_table:
+            for record in records:
+                if record >> 12 == IMAGE_REL_BASED_HIGHLOW:
+                    yield cur_page | (record & 0x0FFF)
+
+    @staticmethod
+    def _read_raw_table(file, offset, size):
+        file.seek(offset)
+        cur_off = 0
+        while cur_off < size:
+            cur_page = int.from_bytes(file.read(4), 'little')
+            block_size = int.from_bytes(file.read(4), 'little')
+            assert (block_size > 8)
+            assert ((block_size - 8) % 2 == 0)
+            relocs = array('H').fromfile(file, (block_size - 8) // 2)
+            yield cur_page, relocs
+            cur_off += block_size
+
+    @classmethod
+    def read(cls, file, offset, size):
+        raw_table = cls._read_raw_table(file, offset, size)
+        return cls(raw_table)
+
+
 class Pe:
     def __init__(self, file):
+        self.file = file
         self.dos_header = ImageDosHeader(file)
         self.nt_headers = ImageNTHeaders(file, self.dos_header.e_lfanew)
         self.file_header = self.nt_headers.file_header
         self.optional_header = self.nt_headers.optional_header
         self.data_directory = self.optional_header.data_directory
         self._section_table = None
+        self._relocation_table = None
 
     @property
     def section_table(self):
         if self._section_table is None:
             n = self.file_header.number_of_sections
             offset = self.nt_headers.offset + self.nt_headers.size
-            self._section_table = SectionTable.read(offset, n)
+            self._section_table = SectionTable.read(self.file, offset, n)
         return self._section_table
+
+    @property
+    def relocation_table(self):
+        if self._relocation_table is None:
+            rva = self.data_directory.basereloc.virtual_address
+            offset = self.section_table.rva_to_offset(rva)
+            size = self.data_directory.basereloc.size
+            self._relocation_table = RelocationTable.read(self.file, offset, size)
+        return self._relocation_table
 
     def info(self):
         return (
