5e9084fe2fb8a747d5bbf602879a5e54fc8e064c
Mike Manturov <insolor@users.noreply.github.com>
2015-09-09 14:28:30 +0300

    Remove extra empty line

--- peclasses.py
+++ peclasses.py
@@ -59,7 +59,6 @@ class ImageOptionalHeader:
         'magic', 'major_linker_version', 'minor_linker_version', 'size_of_code',
         'size_of_initialized_data', 'size_of_uninitialized_data', 'address_of_entry_point', 'base_of_code',
         'base_of_data', 'image_base', 'section_alignment', 'file_alignment',
-
         'major_operating_system_version', 'minor_operating_system_version',
         'major_image_version', 'minor_image_version',
         'major_subsystem_version', 'minor_subsystem_version',
