3c63a3fb88a063b13f9bc8bda7e2961486a00ba2
Insolor <Insolor@localhost>
2015-09-12 23:10:34 +0300

    Add diffing functions for the section table

--- pediff.py
+++ pediff.py
@@ -24,11 +24,22 @@ def main():
                 changed = getattr(pe2, structure)
                 changes = list(original.diff(changed))
                 if not changes:
-                    print(' No changes')
+                    print('  No changes')
                 else:
                     for field, formatter, change in changes:
-                        print(' - %s=%s' % (field, formatter) % change[0])
-                        print(' + %s=%s' % (field, formatter) % change[1])
+                        print('- %s=%s' % (field, formatter) % change[0])
+                        print('+ %s=%s' % (field, formatter) % change[1])
+
+            print('Section table:')
+            changes = pe1.section_table.diff(pe2.section_table)
+            if not changes:
+                print('  No changes')
+            else:
+                for left, right in changes:
+                    if left is not None:
+                        print('-', left)
+                    if right is not None:
+                        print('+', right)
 
 if __name__ == "__main__":
     main()
