5961ee4f2b6ae8ac8c6c4f8d0591750608afb938
Mike Manturov <insolor@users.noreply.github.com>
2016-01-23 13:43:24 +0300

    Show some messages in edit_relocs.py

--- edit_relocs.py
+++ edit_relocs.py
@@ -68,7 +68,12 @@ else:
                     print('"-*" operation needs only 2 arguments. Using only two of them: 0x%x, 0x%x.' %
                           tuple(items[:2]))
                 lower_bound, upper_bound = items[:2]
-                relocs = set(filter(lambda x: not (lower_bound <= x <= upper_bound), relocs))
+                relocs_in_range = list(filter(lambda x: lower_bound <= x <= upper_bound, relocs))
+                if not relocs_in_range:
+                    print("No relocations in range.")
+                else:
+                    print("These relocations will be removed: %s" % ', '.join(hex(x) for x in sorted(relocs_in_range)))
+                    relocs = set(filter(lambda x: not (lower_bound <= x <= upper_bound), relocs))
             else:
                 print('Wrong operation: "%s". Skipped.' % cmd[2])
 
