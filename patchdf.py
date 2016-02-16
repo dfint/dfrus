791d2063655e2fc6d5a2a53aae7f79407d8687e2
Insolor <Insolor@localhost>
2016-02-17 01:19:08 +0300

    Remove crutches

--- patchdf.py
+++ patchdf.py
@@ -422,8 +422,6 @@ def fix_len(fn, offset, oldlen, newlen, new_str_rva):
                 else:
                     meta['fixed'] = 'no'
                     return meta
-            else:
-                meta['temp'] = offset
         elif reg == Reg.esi:
             # Sample code:
             # ; oldlen = 22
