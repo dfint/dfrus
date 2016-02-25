e98458a691f15c2303f7380c85f40730f0e487ba
Mike Manturov <insolor@gmail.com>
2016-02-26 01:22:36 +0300

    Skip not allowed characters instead of breaking

--- test_extract_strings.py
+++ test_extract_strings.py
@@ -5,3 +5,8 @@ from extract_strings import check_string_array
 def test_check_string_array():
     assert (list(check_string_array(b'\0\0\0abcd\0\0\0\0foo\0\0\0\0\0\0\0bar\0\0qwerty\0\0', 0)) ==
         [(3, b'abcd', 7), (11, b'foo', 9), (21, b'bar', 4), (26, b'qwerty', 7)])
+
+def test_check_string_array_1():
+    assert (list(check_string_array(b'\0\0\1abcd\0\0\0\2foo\0\0\0\0\0\2\3bar\0\4qwerty\0\0', 0)) ==
+        [(3, b'abcd', 6), (11, b'foo', 7), (21, b'bar', 3), (26, b'qwerty', 7)])
+
