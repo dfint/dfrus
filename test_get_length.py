9828281a09f4d76438baceac6ceb5c10b5a3d85a
Mike Manturov <insolor@users.noreply.github.com>
2016-02-29 01:52:10 +0300

    Added support of string copiyng at absolute address, and various other fixes.

--- test_get_length.py
+++ test_get_length.py
@@ -20,7 +20,8 @@ def test_get_length():
     result = get_length(test_data_1, 7)
     result['dest'] = str(result['dest'])
     assert result == dict(
-        deleted={2, 9},
+        deleted_relocs={2, 9},
+        added_relocs=set(),
         dest='[esp+20h]',
         length=21,
         saved_mach=b'\x8b\xf0'  # mov esi, eax
@@ -50,8 +51,45 @@ def test_get_length_push():
     result = get_length(test_data_push, 15)
     result['dest'] = str(result['dest'])
     assert result == dict(
-        deleted={2, 7, 13, 30},
+        deleted_relocs={2, 7, 13, 30},
+        added_relocs=set(),
         dest='[ecx]',
         length=53,
         saved_mach=bytes.fromhex('8D 8D 90 FB FF FF  51')  # lea ecx, [ebp-470h] ; push ecx
+    )
+
+
+# 414111 !   mov         edx, [strz_after_a_while_543e38]
+# 414117 !   mov         eax, [strz_r_a_while_543e3c]
+# 41411c !   sub         ecx, [?data_ad682e0]
+# 414122 !   mov         [?data_617990], edx
+# 414128 !   mov         edx, [strz_while_543e40]
+# 41412e !   mov         [?data_617994], eax
+# 414133 !   mov         ax, [data_543e44]
+# 414139 !   mov         [?data_617998], edx
+# 41413f !   mov         [?data_61799c], ax
+# 414145 !   cmp         ecx, 0fh
+test_data_sub = bytes.fromhex(
+    '8B 15 38 3E 54 00 '
+    'A1 3C 3E 54 00 '
+    '2B 0D E0 82 D6 0A '
+    '89 15 90 79 61 00 '
+    '8B 15 40 3E 54 00 '
+    'A3 94 79 61 00 '
+    '66 A1 44 3E 54 00 '
+    '89 15 98 79 61 00 '
+    '66 A3 9C 79 61 00 '
+    '83 F9 0F '
+)
+
+
+def test_get_length_sub():
+    result = get_length(test_data_sub, 13)
+    result['dest'] = str(result['dest'])
+    assert result == dict(
+        deleted_relocs={2, 7, 13, 19, 25, 30, 36, 42, 48},
+        added_relocs={2},
+        dest='[617990h]',
+        length=52,
+        saved_mach=bytes.fromhex('2B 0D E0 82 D6 0A ')  # sub ecx, [0ad682e0h]
     )
