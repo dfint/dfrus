98a779b6c069880469caf7ce6c15f38db7e8f929
Mike Manturov <insolor@users.noreply.github.com>
2016-02-29 05:53:14 +0300

    Fix data output for movsw instruction from disasm

--- test_disasm.py
+++ test_disasm.py
@@ -4,3 +4,8 @@ from disasm import disasm
 def test_mov_al_1():
     s = str(next(disasm(bytes.fromhex('B0 01'))))
     assert s == 'mov al, 1'
+
+
+def test_movsw():
+    data = bytes.fromhex('66 A5')  # movsw
+    assert next(disasm(data)).data == data
