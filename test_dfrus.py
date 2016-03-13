7c7befbb119a20dfa9a2837c8fe472dd3eb3c4a8
Mike Manturov <insolor@users.noreply.github.com>
2016-03-13 23:36:18 +0300

    Another test for find_earliest_midrefs() will not be superfluous.

--- test_dfrus.py
+++ test_dfrus.py
@@ -11,3 +11,12 @@ def test_find_earliest_midrefs_beater():
         offset+6: [0x44eeb4],
     }
     assert find_earliest_midrefs(offset, xref_table[offset], xref_table, len('Beater')) == [0x44eeb4, 0x4549b7, 0x4551A1]
+
+
+def test_find_earliest_midrefs_sword():
+    offset = 0x54A44C
+    xref_table = {
+        offset: [0x4a306b, 0x496c85, 0x49eb2f],
+        offset+4: [0x4a3065, 0x496c78, 0x49eb2b],
+    }
+    assert find_earliest_midrefs(offset, xref_table[offset], xref_table, len('SWORD')) == [0x4a3065, 0x496c78, 0x49eb2b]
