0f2bad9ac647a74a5a72cc04afa5cee237412284
insolor <insolor@localhost>
2016-08-27 02:59:08 +0300

    Fix search of charmap for DF 0.43.04 and higher

--- dfrus.py
+++ dfrus.py
@@ -89,6 +89,24 @@ def find_earliest_midrefs(off, refs, xref_table, length):
     return refs
 
 
+def search_charmap(fn, sections, xref_table):
+    unicode_table_start = b''.join(
+        to_dword(item) for item in [0x20, 0x263A, 0x263B, 0x2665, 0x2666, 0x2663, 0x2660, 0x2022]
+    )
+    
+    offset = sections[1].physical_offset
+    size = sum(section.physical_size for section in sections[1:])
+    data_block = fpeek(fn, offset, size)
+    for obj_off in xref_table:
+        off = obj_off - offset
+        if 0 <= off < size:
+            buf = data_block[off:off+len(unicode_table_start)]
+            if buf == unicode_table_start:
+                return obj_off
+    
+    return None
+
+
 def _main():
     parser = init_argparser()
 
@@ -218,21 +236,9 @@ def _main():
     # --------------------------------------------------------
     if args.codepage:
         print("Searching for charmap table...")
-        unicode_table_start = b''.join(
-            to_dword(item) for item in [0x20, 0x263A, 0x263B, 0x2665, 0x2666, 0x2663, 0x2660, 0x2022]
-        )
+        needle = search_charmap(fn, sections, xref_table)
         
-        physical_offset = sections[data].physical_offset
-        physical_size = sections[data].physical_size
-        data_section = fpeek(fn, physical_offset, physical_size)
-        for obj_off in xref_table:
-            off = obj_off - physical_offset
-            if 0 <= off < physical_size:
-                buf = data_section[off:off+len(unicode_table_start)]
-                if buf == unicode_table_start:
-                    needle = obj_off
-                    break
-        else:
+        if needle is None:
             fn.close()
             print("Charmap table not found.")
             sys.exit()
