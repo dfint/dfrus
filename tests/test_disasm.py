923e12653d8b1de61021c60b3c1d4c49dc88b828
insolor <insolor@localhost>
2017-05-04 00:26:13 +0300

    Partially fix disasm (now all the test_disasm.py tests are passed)

--- tests/test_disasm.py
+++ tests/test_disasm.py
@@ -1,4 +1,4 @@
-from dfrus.disasm import disasm, analyse_modrm
+from dfrus.disasm import disasm, analyse_modrm, ModRM, Sib
 
 
 def test_mov_al_1():
@@ -30,3 +30,12 @@ def test_movups():
     data = bytes.fromhex('0f 11 02')
     d = next(disasm(data))
     assert str(d) == 'movups [edx], xmm0'
+
+
+def test_analyse_modrm():
+    data = bytes.fromhex('0c8dc0eed00a')
+    assert (analyse_modrm(data, 0) ==
+            (dict(modrm=ModRM(mode=0, reg=1, regmem=4),
+                  sib=Sib(scale=2, index_reg=1, base_reg=5),
+                  disp=0x0AD0EEC0),
+             len(data)))
