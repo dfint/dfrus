a560925cc49d77ea9488337c940fedce14deeab6
insolor <insolor@gmail.com>
2020-09-29 10:51:28 +0300

    Some comments in the relocation table test

--- tests/test_relocation.py
+++ tests/test_relocation.py
@@ -6,15 +6,17 @@ from dfrus.peclasses import RelocationTable
 def test_relocation():
     relocs = [0x123456, 0xdeadbeef]
     table = RelocationTable.build(relocs)
-    print(table._table)
+    
     # Test __iter__ method
     assert list(table) == relocs
     
     file = io.BytesIO()
     table.to_file(file)
+    
+    # Check if size of the table written to the file corresponds with the calculated size
     assert len(file.getbuffer()) == table.size
     
-    # Test if the table written to a file is the same as before
+    # Reread table and compare result to the original data
     file.seek(0)
     table = RelocationTable.from_file(file, len(file.getbuffer()))
     
