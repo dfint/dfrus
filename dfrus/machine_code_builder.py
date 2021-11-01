d70cdcf0b9a79db02713f741de3aa86c96b19bd5
insolor <insolor@gmail.com>
2021-11-01 16:09:41 +0300

    Add tests, fixes

--- dfrus/machine_code_builder.py
+++ dfrus/machine_code_builder.py
@@ -1,40 +1,41 @@
 """
 Machine code builder. Possible replacement for the MachineCode class.
-See concept() for example.
+
+# Concept:
+m = MachineCodeBuilder()
+m.byte(mov_rm_imm | 1).byte(join_byte(1, 0, Reg.esi)).byte(0x14).dword(0xf)  # mov dword [esi+14h], 0fh
+m.byte(call_near).relative_reference(name='func', size=4)  # call near func
+m.byte(mov_reg_imm | 8 | Reg.edi.code).dword(0xf)  # mov edi, 0fh
+m.byte(jmp_near).relative_reference(name='return_address', size=4)  # jmp near return_addr
+
+m.origin_address = 0x123456
+m.values(func=0x756733, return_address=0x475675)
+
+file = io.BytesIO()
+file.seek(m.origin_address)
+file.write(m.build())
 """
 import io
-from typing import List, Optional, Dict, Mapping
+from typing import List, Optional, Dict, Mapping, Union, Iterable
 
 from attr import dataclass
 
 from .binio import to_unsigned
 
 
-def concept():
-    from .disasm import join_byte
-    from .opcodes import mov_rm_imm, Reg, mov_reg_imm, call_near, jmp_near
-
-    # Concept:
-    m = MachineCodeBuilder()
-    m.byte(mov_rm_imm | 1).byte(join_byte(1, 0, Reg.esi)).byte(0x14).dword(0xf)  # mov dword [esi+14h], 0fh
-    m.byte(call_near).relative_reference(name='func', size=4)  # call near func
-    m.byte(mov_reg_imm | 8 | Reg.edi.code).dword(0xf)  # mov edi, 0fh
-    m.byte(jmp_near).relative_reference(name='return_address', size=4)  # jmp near return_addr
-
-    m.origin_address = 0x123456
-    m.values(func=0x756733, return_address=0x475675)
-
-    file = io.BytesIO()
-    file.seek(m.origin_address)
-    file.write(m.build())
-
-
 @dataclass
 class MachineCodeItem:
     size: int
     name: Optional[str] = None
-    value: Optional[int] = None
+    value: Union[int, bytes] = None
     position: Optional[int] = None
+
+    """
+    :is_relative
+    * True for relative reverence,
+    * False for absolute reference,
+    * None for everything else
+    """
     is_relative: Optional[bool] = None
 
 
@@ -53,25 +54,38 @@ class MachineCodeBuilder:
         self._cursor += self._raw_list[-1].size
         return self
 
-    def byte(self, value) -> "MachineCodeBuilder":
+    def byte(self, value: int) -> "MachineCodeBuilder":
         return self._add_item(MachineCodeItem(value=value, size=1))
 
-    def dword(self, value) -> "MachineCodeBuilder":
+    def dword(self, value: int) -> "MachineCodeBuilder":
         return self._add_item(MachineCodeItem(value=value, size=4))
 
-    def label(self, name) -> "MachineCodeBuilder":
+    def label(self, name: str) -> "MachineCodeBuilder":
         self._labels[name] = self._cursor
         return self
 
-    def relative_reference(self, name, size) -> "MachineCodeBuilder":
+    def bytes(self, *args, **kwargs) -> "MachineCodeBuilder":
+        value = bytes(*args, **kwargs)
+        return self._add_item(MachineCodeItem(value=value, size=len(value)))
+
+    def relative_reference(self, name: str, size: int) -> "MachineCodeBuilder":
         reference = MachineCodeItem(name=name, size=size, is_relative=True)
         self._fields[name] = reference
         return self._add_item(reference)
 
-    def absolute_reference(self, name, size) -> "MachineCodeBuilder":
+    def absolute_reference(self, name: str, size: int) -> "MachineCodeBuilder":
         reference = MachineCodeItem(name=name, size=size, is_relative=False)
         self._fields[name] = reference
         return self._add_item(reference)
+
+    @property
+    def absolute_references(self) -> Iterable[int]:
+        """
+        Get addresses of absolute references (to add them to the relocation table)
+        """
+        for item in self._fields.values():
+            if item.is_relative is False:  # is absolute reference
+                yield self.origin_address + item.position
 
     def _set_value(self, field_name: str, value: int) -> None:
         field = self._fields[field_name]
@@ -102,10 +116,13 @@ class MachineCodeBuilder:
             if item.value is None:
                 raise ValueError(f"Value of the {item.name!r} field is undefined")
 
-            value = to_unsigned(item.value, item.size)
-            if item.size == 1:
-                buffer.write(bytes([value]))
+            if isinstance(item.value, bytes):
+                buffer.write(item.value)
             else:
-                buffer.write(value.to_bytes(item.size, signed=False, byteorder=self.byteorder))
+                value = to_unsigned(item.value, item.size * 8)
+                if item.size == 1:
+                    buffer.write(bytes([value]))
+                else:
+                    buffer.write(value.to_bytes(item.size, signed=False, byteorder=self.byteorder))
 
         return buffer.getvalue()
---
+++ tests/test_machinecode_builder.py
@@ -0,0 +1,55 @@
+from dfrus.disasm import join_byte
+from dfrus.machine_code_builder import MachineCodeBuilder
+from dfrus.opcodes import mov_rm_imm, Reg, call_near, mov_reg_imm, jmp_near
+
+
+def test_machinecode_1():
+    """
+    # Sample code:
+    use32
+
+    func = 222222h
+    return_addr = 777777h
+
+    org 123456h
+
+    mov dword [esi+14h], 0fh
+    call near func
+    mov edi, 0fh
+    jmp near return_addr
+    """
+
+    m = MachineCodeBuilder()
+    m.byte(mov_rm_imm | 1).byte(join_byte(1, 0, Reg.esi)).byte(0x14).dword(0xf)  # mov dword [esi+14h], 0fh
+    m.byte(call_near).relative_reference("func", size=4)  # call near func
+    m.byte(mov_reg_imm | 8 | Reg.edi.code).dword(0xf)  # mov edi, 0fh
+    m.byte(jmp_near).relative_reference("return_addr", size=4)  # jmp near return_addr
+
+    m.origin_address = 0x123456
+    m.values(func=0x222222, return_addr=0x777777)
+
+    assert m.build() == bytes.fromhex('C7 46 14 0F 00 00 00 E8 C0 ED 0F 00 BF 0F 00 00 00 E9 0B 43 65 00')
+
+
+def test_machinecode_2():
+    # Test getting addresses of absolute references
+    m = MachineCodeBuilder()
+    m.bytes(123)
+    m.absolute_reference("b", size=4)
+    m.bytes(12345)
+    m.absolute_reference("a", size=4)
+    m.bytes(10)
+
+    m.origin_address = 0
+    m.values(a=0xDEAD, b=0xBEEF)
+
+    b = m.build()
+    field_values = m.values()
+    found_refs = {b.index(field_values[ref_name].to_bytes(4, 'little')) for ref_name in 'ab'}
+    assert found_refs == set(m.absolute_references)
+
+
+# def test_mach_strlen():
+#     # Test the new mach_strlen:
+#     code = mach_strlen(nop)
+#     assert bytes(code) == bytes.fromhex('51 31 C9 80 3C 08 00 74 0B 81 F9 00 01 00 00 7F 04 41 EB EF 90 59')
