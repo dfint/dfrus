fd21f3d23814b3f2a4ee6bf9a59bea1e2d77a446
insolor <insolor@gmail.com>
2021-11-24 08:48:14 +0300

    Add handlers to the logger

--- dfrus/logger.py
+++ dfrus/logger.py
@@ -8,6 +8,15 @@ def get_logger() -> logging.Logger:
     logger = logging.Logger(name="dfrus")
 
     file_handler = RotatingFileHandler("dfrus.log", maxBytes=1024**2, backupCount=1, encoding="utf-8")
+
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s "
+                                  "(%(filename)s).%(funcName)s(%(lineno)d): %(message)s")
+
+    file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
 
     return logger
+
+
+def get_stream_handler(stream) -> logging.StreamHandler:
+    return logging.StreamHandler(stream)
